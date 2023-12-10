b21aaf9996dc82b414a5a17d03812691b4df4d50
Eduardo Felizardo Cândido <meugit.edufelizardo@gmail.com>
2023-12-10 10:47:47 -0300

    Base um

---
+++ API-Mais-Saude-Publica/src/main/java/com/edufelizardo/API/Mais/Saude/Publica/ApiMaisSaudePublicaApplication.java
@@ -0,0 +1,13 @@
+package com.edufelizardo.API.Mais.Saude.Publica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMaisSaudePublicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMaisSaudePublicaApplication.class, args);
+	}
+
+}
---
+++ API-Mais-Saude-Publica/src/test/java/com/edufelizardo/API/Mais/Saude/Publica/ApiMaisSaudePublicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edufelizardo.API.Mais.Saude.Publica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMaisSaudePublicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
