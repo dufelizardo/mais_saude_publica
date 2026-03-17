050be5e06ce4edd7ee4a72f79a27dd43136b76aa
Eduardo Felizardo Cândido <meugit.edufelizardo@gmail.com>
2026-03-17 10:44:05 -0300

    developer

---
+++ src/main/java/br/com/eduafelizardo/mais_saude_publica/service/IConverteDados.java
@@ -0,0 +1,7 @@
+package br.com.eduafelizardo.mais_saude_publica.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+public interface IConverteDados {
+    <T> T obterDados(String json, Class<T> classe) throws JsonProcessingException;
+}
